715943bbd1c9c9a714a89a7ef80f4eee2f8cfdfe
MIN GEUN <min0903@adcapsule.co.kr>
2020-03-25 15:02:00 +0900

    #2.1 2D Context

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 const convas = document.getElementById("jsCanvas");
+const ctx = convas.getContext("2d");
+
+ctx.strokeStyle ="#2c2c2c";
+ctx.lineWidth = 2.5;
+
 let painting = false;
 
 function stopPainting() {
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }else {
+        ctx.lineTo(x,y);
+    }
 }
 
 function onMouseDown(evnet){
-
     painting = true;
-    console.log(event);
-}
-
-function onMouseUp(event){
-    paintig = true;
-}
-
-function onMouseLeave(event){
-    stopPainting
 }
 
 if(convas){
     convas.addEventListener("mousemove", onMouseMove);
-    convas.addEventListener("mousedown", onMouseDown) //mousedown: 클릭했을때 발생하는 event.
-    convas.addEventListener("mouseup", onMouseUp);
+    convas.addEventListener("mousedown", startPainting) //mousedown: 클릭했을때 발생하는 event.
+    convas.addEventListener("mouseup", stopPainting);
     convas.addEventListener("mouseleave", stopPainting);
 }
