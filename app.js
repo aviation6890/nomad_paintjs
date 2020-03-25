48a5d879731c42ccc1420219abe7ce8890c3da12
MIN GEUN <min0903@adcapsule.co.kr>
2020-03-25 17:20:19 +0900

    #2.6 Saving the image

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -68,12 +72,27 @@ function handleCanvasClick(event){
     }
 }
 
+function handleCM(evnent){
+    console.log(event);
+    event.preventDefault();
+}
+
+function handleSaveClick(event){
+    const image = canvas.toDataURL("");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[EXPORT]";
+    link.click();
+
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting) //mousedown: 클릭했을때 발생하는 event.
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 if(colors){
@@ -88,5 +107,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click",handleModeClick);
+}
 
+if(saveBtn){
+    saveBtn.addEventListener("click",handleSaveClick);
 }
